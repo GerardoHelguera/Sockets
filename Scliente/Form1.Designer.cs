7b13efd54ac89ed9a941dbb36d0f38b2b2739430
GerardoHelguera <72096620+GerardoHelguera@users.noreply.github.com>
2020-11-01 00:20:40 -0600

    Add files via upload

---
+++ Scliente/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Scliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtUs = new System.Windows.Forms.TextBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(76, 91);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 23);
+            this.btnIn.TabIndex = 1;
+            this.btnIn.Text = "Iniciar";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(275, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 169);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtUs
+            // 
+            this.txtUs.Location = new System.Drawing.Point(44, 62);
+            this.txtUs.Name = "txtUs";
+            this.txtUs.Size = new System.Drawing.Size(148, 23);
+            this.txtUs.TabIndex = 3;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(44, 172);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(144, 23);
+            this.txtMensaje.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User";
+            // 
+            // btnEn
+            // 
+            this.btnEn.Location = new System.Drawing.Point(194, 171);
+            this.btnEn.Name = "btnEn";
+            this.btnEn.Size = new System.Drawing.Size(75, 23);
+            this.btnEn.TabIndex = 6;
+            this.btnEn.Text = "Enviar";
+            this.btnEn.UseVisualStyleBackColor = true;
+            this.btnEn.Click += new System.EventHandler(this.btnEn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 197);
+            this.Controls.Add(this.btnEn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtUs);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnIn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtUs;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEn;
+    }
+}
+
---
+++ Scliente/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Scliente
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream corr;
+        static private StreamWriter esc;
+        static private StreamReader lec;
+        static private TcpClient User = new TcpClient();
+        static private string name = "unknown";
+
+        private delegate void DAddItem(String i);
+
+        private void AddItem(String i)
+        {
+            listBox1.Items.Add(i);
+        }
+
+        void Listen()
+        {
+            while (User.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DAddItem(AddItem), lec.ReadLine());
+
+                }
+                catch
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void btnEn_Click(object sender, EventArgs e)
+        {
+
+            esc.WriteLine(txtMensaje.Text);
+            esc.Flush();
+            txtUs.Clear();
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            btnIn.Visible = false;
+            label1.Visible = false;
+            txtUs.Visible = false;
+            name = txtUs.Text;
+
+            try
+            {
+                User.Connect("127.0.0.1", 8000);
+                switch (User.Connected)
+                {
+                    case true:
+                        Thread hilo = new Thread(Listen);
+
+                        corr = User.GetStream();
+                        esc = new StreamWriter(corr);
+                        lec = new StreamReader(corr);
+
+                        esc.WriteLine(name);
+                        esc.Flush();
+
+                        hilo.Start();
+
+                        break;
+                    default:
+
+                        MessageBox.Show("Fue imposible conectar con el servidor");
+                        Application.Exit();
+                        break;
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fue imposible conectar con el servidor");
+                Application.Exit();
+            }
+        }
+    }
+}
